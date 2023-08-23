45514e132490daba10f59015c9fa670c12becf74
Max R <mxr@users.noreply.github.com>
2023-08-23 14:55:05 -0400

    Sync pre-commit deps with other pre-commit libraries

--- sync_pre_commit_deps.py
+++ sync_pre_commit_deps.py
@@ -5,17 +5,12 @@ from collections.abc import Sequence
 
 import ruamel.yaml
 
-SUPPORTED = frozenset(
-    (
-        "black",
-        "flake8",
-    )
-)
+SUPPORTED = frozenset(('black', 'flake8'))
 
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", default=".pre-commit-config.yaml")
+    parser.add_argument('filename', default='.pre-commit-config.yaml')
 
     args = parser.parse_args(argv)
     filename: str = args.filename
@@ -31,32 +26,33 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     # TODO - validate schema?
     versions = {}
-    for repo in loaded["repos"]:
-        for hook in repo["hooks"]:
-            if (hid := hook["id"]) in SUPPORTED:
-                versions[hid] = repo["rev"]
+    for repo in loaded['repos']:
+        for hook in repo['hooks']:
+            if (hid := hook['id']) in SUPPORTED:
+                versions[hid] = repo['rev']
 
     updated = []
-    for repo in loaded["repos"]:
-        for hook in repo["hooks"]:
-            for i, dep in enumerate(hook.get("additional_dependencies", ())):
-                name, _, cur_version = dep.partition("==")
+    for repo in loaded['repos']:
+        for hook in repo['hooks']:
+            for i, dep in enumerate(hook.get('additional_dependencies', ())):
+                name, _, cur_version = dep.partition('==')
                 target_version = versions.get(name, cur_version)
                 if target_version != cur_version:
-                    hook["additional_dependencies"][i] = f"{name}=={target_version}"
-                    updated.append((hook["id"], name))
+                    name_and_version = f'{name}=={target_version}'
+                    hook['additional_dependencies'][i] = name_and_version
+                    updated.append((hook['id'], name))
 
     if updated:
-        print(f"Writing updates to {filename}:")
+        print(f'Writing updates to {filename}:')
         for hid, name in updated:
-            print(f"\tSetting {hid!r} dependency {name!r} to {versions[name]}")
+            print(f'\tSetting {hid!r} dependency {name!r} to {versions[name]}')
 
-        with open(filename, "w+") as f:
+        with open(filename, 'w+') as f:
             yaml.dump(loaded, f)
         return 1
 
     return 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     raise SystemExit(main())
--- tests/sync_pre_commit_deps_test.py
+++ tests/sync_pre_commit_deps_test.py
@@ -5,19 +5,19 @@ from sync_pre_commit_deps import main
 
 def test_main_noop(tmpdir):
     s = (
-        "repos:\n"
-        "-   repo: https://github.com/psf/black\n"
-        "    rev: 23.3.0\n"
-        "    hooks:\n"
-        "    -   id: black\n"
-        "-   repo: https://github.com/adamchainz/blacken-docs\n"
-        "    rev: 1.15.0\n"
-        "    hooks:\n"
-        "    -   id: blacken-docs\n"
-        "        additional_dependencies:\n"
-        "        -   black==23.3.0\n"
+        'repos:\n'
+        '-   repo: https://github.com/psf/black\n'
+        '    rev: 23.3.0\n'
+        '    hooks:\n'
+        '    -   id: black\n'
+        '-   repo: https://github.com/adamchainz/blacken-docs\n'
+        '    rev: 1.15.0\n'
+        '    hooks:\n'
+        '    -   id: blacken-docs\n'
+        '        additional_dependencies:\n'
+        '        -   black==23.3.0\n'
     )
-    cfg = tmpdir.join(".pre-commit-config.yaml")
+    cfg = tmpdir.join('.pre-commit-config.yaml')
     cfg.write(s)
 
     assert not main((str(cfg),))
@@ -26,126 +26,126 @@ def test_main_noop(tmpdir):
 
 
 def test_main_writes_both(tmpdir):
-    cfg = tmpdir.join(".pre-commit-config.yaml")
+    cfg = tmpdir.join('.pre-commit-config.yaml')
     cfg.write(
-        "repos:\n"
+        'repos:\n'
         # should not be touched
-        "-   repo: https://github.com/asottile/pyupgrade\n"
-        "    rev: v3.8.0\n"
-        "    hooks:\n"
-        "    -   id: pyupgrade\n"
+        '-   repo: https://github.com/asottile/pyupgrade\n'
+        '    rev: v3.8.0\n'
+        '    hooks:\n'
+        '    -   id: pyupgrade\n'
         # gives the `black` version
-        "-   repo: https://github.com/psf/black\n"
-        "    rev: 23.3.0\n"
-        "    hooks:\n"
-        "    -   id: black\n"
+        '-   repo: https://github.com/psf/black\n'
+        '    rev: 23.3.0\n'
+        '    hooks:\n'
+        '    -   id: black\n'
         # gives the `flake8` version
-        "-   repo: https://github.com/PyCQA/flake8\n"
-        "    rev: 6.0.0\n"
-        "    hooks:\n"
-        "    -   id: flake8\n"
+        '-   repo: https://github.com/PyCQA/flake8\n'
+        '    rev: 6.0.0\n'
+        '    hooks:\n'
+        '    -   id: flake8\n'
         # all 3 below should be rewritten
-        "-   repo: https://github.com/asottile/yesqa\n"
-        "    rev: v1.5.0\n"
-        "    hooks:\n"
-        "    -   id: yesqa\n"
-        "        additional_dependencies:\n"
-        "        -   flake8==5.0.0\n"
-        "-   repo: https://github.com/adamchainz/blacken-docs\n"
-        "    rev: 1.15.0\n"
-        "    hooks:\n"
-        "    -   id: blacken-docs\n"
-        "        additional_dependencies:\n"
-        "        -   black==22.12.0\n"
-        "-   repo: https://github.com/example/example\n"
-        "    rev: v1.0.0\n"
-        "    hooks:\n"
-        "    -   id: example\n"
-        "        additional_dependencies:\n"
-        "        -   black==22.12.0\n"
-        "        -   flake8==5.0.0\n"
+        '-   repo: https://github.com/asottile/yesqa\n'
+        '    rev: v1.5.0\n'
+        '    hooks:\n'
+        '    -   id: yesqa\n'
+        '        additional_dependencies:\n'
+        '        -   flake8==5.0.0\n'
+        '-   repo: https://github.com/adamchainz/blacken-docs\n'
+        '    rev: 1.15.0\n'
+        '    hooks:\n'
+        '    -   id: blacken-docs\n'
+        '        additional_dependencies:\n'
+        '        -   black==22.12.0\n'
+        '-   repo: https://github.com/example/example\n'
+        '    rev: v1.0.0\n'
+        '    hooks:\n'
+        '    -   id: example\n'
+        '        additional_dependencies:\n'
+        '        -   black==22.12.0\n'
+        '        -   flake8==5.0.0\n',
     )
 
     assert main((str(cfg),))
 
     assert cfg.read() == (
-        "repos:\n"
-        "-   repo: https://github.com/asottile/pyupgrade\n"
-        "    rev: v3.8.0\n"
-        "    hooks:\n"
-        "    -   id: pyupgrade\n"
-        "-   repo: https://github.com/psf/black\n"
-        "    rev: 23.3.0\n"
-        "    hooks:\n"
-        "    -   id: black\n"
-        "-   repo: https://github.com/PyCQA/flake8\n"
-        "    rev: 6.0.0\n"
-        "    hooks:\n"
-        "    -   id: flake8\n"
-        "-   repo: https://github.com/asottile/yesqa\n"
-        "    rev: v1.5.0\n"
-        "    hooks:\n"
-        "    -   id: yesqa\n"
-        "        additional_dependencies:\n"
-        "        -   flake8==6.0.0\n"
-        "-   repo: https://github.com/adamchainz/blacken-docs\n"
-        "    rev: 1.15.0\n"
-        "    hooks:\n"
-        "    -   id: blacken-docs\n"
-        "        additional_dependencies:\n"
-        "        -   black==23.3.0\n"
-        "-   repo: https://github.com/example/example\n"
-        "    rev: v1.0.0\n"
-        "    hooks:\n"
-        "    -   id: example\n"
-        "        additional_dependencies:\n"
-        "        -   black==23.3.0\n"
-        "        -   flake8==6.0.0\n"
+        'repos:\n'
+        '-   repo: https://github.com/asottile/pyupgrade\n'
+        '    rev: v3.8.0\n'
+        '    hooks:\n'
+        '    -   id: pyupgrade\n'
+        '-   repo: https://github.com/psf/black\n'
+        '    rev: 23.3.0\n'
+        '    hooks:\n'
+        '    -   id: black\n'
+        '-   repo: https://github.com/PyCQA/flake8\n'
+        '    rev: 6.0.0\n'
+        '    hooks:\n'
+        '    -   id: flake8\n'
+        '-   repo: https://github.com/asottile/yesqa\n'
+        '    rev: v1.5.0\n'
+        '    hooks:\n'
+        '    -   id: yesqa\n'
+        '        additional_dependencies:\n'
+        '        -   flake8==6.0.0\n'
+        '-   repo: https://github.com/adamchainz/blacken-docs\n'
+        '    rev: 1.15.0\n'
+        '    hooks:\n'
+        '    -   id: blacken-docs\n'
+        '        additional_dependencies:\n'
+        '        -   black==23.3.0\n'
+        '-   repo: https://github.com/example/example\n'
+        '    rev: v1.0.0\n'
+        '    hooks:\n'
+        '    -   id: example\n'
+        '        additional_dependencies:\n'
+        '        -   black==23.3.0\n'
+        '        -   flake8==6.0.0\n'
     )
 
 
 def test_main_no_dep_on_one_and_writes_other(tmpdir):
-    cfg = tmpdir.join(".pre-commit-config.yaml")
+    cfg = tmpdir.join('.pre-commit-config.yaml')
     cfg.write(
-        "repos:\n"
-        "-   repo: https://github.com/psf/black\n"
-        "    rev: 23.3.0\n"
-        "    hooks:\n"
-        "    -   id: black\n"
-        "-   repo: https://github.com/asottile/yesqa\n"
-        "    rev: v1.5.0\n"
-        "    hooks:\n"
-        "    -   id: yesqa\n"
-        "        additional_dependencies:\n"
+        'repos:\n'
+        '-   repo: https://github.com/psf/black\n'
+        '    rev: 23.3.0\n'
+        '    hooks:\n'
+        '    -   id: black\n'
+        '-   repo: https://github.com/asottile/yesqa\n'
+        '    rev: v1.5.0\n'
+        '    hooks:\n'
+        '    -   id: yesqa\n'
+        '        additional_dependencies:\n'
         # should not be rewritten because target version can't be found
-        "        -   flake8==5.0.0\n"
-        "-   repo: https://github.com/adamchainz/blacken-docs\n"
-        "    rev: 1.15.0\n"
-        "    hooks:\n"
-        "    -   id: blacken-docs\n"
-        "        additional_dependencies:\n"
+        '        -   flake8==5.0.0\n'
+        '-   repo: https://github.com/adamchainz/blacken-docs\n'
+        '    rev: 1.15.0\n'
+        '    hooks:\n'
+        '    -   id: blacken-docs\n'
+        '        additional_dependencies:\n'
         # should be rewritten
-        "        -   black==22.12.0\n"
+        '        -   black==22.12.0\n',
     )
 
     assert main((str(cfg),))
 
     assert cfg.read() == (
-        "repos:\n"
-        "-   repo: https://github.com/psf/black\n"
-        "    rev: 23.3.0\n"
-        "    hooks:\n"
-        "    -   id: black\n"
-        "-   repo: https://github.com/asottile/yesqa\n"
-        "    rev: v1.5.0\n"
-        "    hooks:\n"
-        "    -   id: yesqa\n"
-        "        additional_dependencies:\n"
-        "        -   flake8==5.0.0\n"
-        "-   repo: https://github.com/adamchainz/blacken-docs\n"
-        "    rev: 1.15.0\n"
-        "    hooks:\n"
-        "    -   id: blacken-docs\n"
-        "        additional_dependencies:\n"
-        "        -   black==23.3.0\n"
+        'repos:\n'
+        '-   repo: https://github.com/psf/black\n'
+        '    rev: 23.3.0\n'
+        '    hooks:\n'
+        '    -   id: black\n'
+        '-   repo: https://github.com/asottile/yesqa\n'
+        '    rev: v1.5.0\n'
+        '    hooks:\n'
+        '    -   id: yesqa\n'
+        '        additional_dependencies:\n'
+        '        -   flake8==5.0.0\n'
+        '-   repo: https://github.com/adamchainz/blacken-docs\n'
+        '    rev: 1.15.0\n'
+        '    hooks:\n'
+        '    -   id: blacken-docs\n'
+        '        additional_dependencies:\n'
+        '        -   black==23.3.0\n'
     )
